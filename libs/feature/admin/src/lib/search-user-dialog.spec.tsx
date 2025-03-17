51036c7302e945609b5f3987751117e23be2d40a
palfnerm <palfner@uni-hildesheim.de>
2025-03-17 10:33:24 +0100

    break pr

--- libs/feature/admin/src/lib/search-user-dialog.spec.tsx
+++ libs/feature/admin/src/lib/search-user-dialog.spec.tsx
@@ -36,6 +36,12 @@ describe("searchUserDialog", () => {
 		};
 	});
 
+	it("shuld fail", () => {
+		const test = true;
+
+		expect(test).toBeFalsy();
+	});
+
 	it("should return user name in onClose", async () => {
 		// Arrange
 		const name = "NotAnAuthor";
